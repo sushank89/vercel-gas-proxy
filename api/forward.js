3f6d0840e5bf33a076b810ccbd7d5adcf090dbe4
sushank89 <37145238+sushank89@users.noreply.github.com>
2025-05-12 11:30:59 +0530

    Update forward.js

--- api/forward.js
+++ api/forward.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
-      const response = await fetch("https://script.google.com/macros/s/AKfycbym57J96D-Yhjt3bEyOoDrrojPedlqqFxG7H62NIaezJLy7dGFXMLAC7VqYlpNtEJW9/exec", {
+      const response = await fetch("https://script.google.com/macros/library/d/1U4mWx1eyqEUDm1dOPzOxw5iDM-eUWca78-u-Z_5vUVQYNga3Fwo1nIwF/2", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(req.body),
