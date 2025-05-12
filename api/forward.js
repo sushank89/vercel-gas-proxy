0badb4389f74cffd5e07d0a885b26c813cd2a6c6
sushank89 <37145238+sushank89@users.noreply.github.com>
2025-05-12 12:02:14 +0530

    Update forward.js

--- api/forward.js
+++ api/forward.js
@@ -1,7 +1,16 @@
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
+  res.setHeader("Access-Control-Allow-Origin", "res.setHeader("Access-Control-Allow-Origin", "https://axomverse.com");
+"); // 
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end(); // Pre-flight request
+  }
+
+  if (req.method === "POST") {
     try {
-      const response = await fetch("https://script.google.com/macros/library/d/1U4mWx1eyqEUDm1dOPzOxw5iDM-eUWca78-u-Z_5vUVQYNga3Fwo1nIwF/2", {
+      const response = await fetch("https://script.google.com/macros/s/AKfycbym57J96D-Yhjt3bEyOoDrrojPedIqqFxG7H62NlaezJLy7dGFXMLAC7VqYlpNtEJW9/exec", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(req.body),
@@ -17,3 +26,4 @@ export default async function handler(req, res) {
     res.status(405).json({ error: "Method not allowed" });
   }
 }
+
