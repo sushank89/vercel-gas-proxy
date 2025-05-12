40a5602ee05358666733c6c861c9e9431e015ea2
sushank89 <37145238+sushank89@users.noreply.github.com>
2025-05-12 11:03:47 +0530

    Create forward.js

---
+++ api/forward.js
@@ -0,0 +1,19 @@
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
+    try {
+      const response = await fetch("https://script.google.com/macros/s/AKfycbym57J96D-Yhjt3bEyOoDrrojPedlqqFxG7H62NIaezJLy7dGFXMLAC7VqYlpNtEJW9/exec", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(req.body),
+      });
+
+      const data = await response.json();
+      res.status(200).json(data);
+    } catch (error) {
+      console.error("Proxy error:", error);
+      res.status(500).json({ error: "Proxy failed" });
+    }
+  } else {
+    res.status(405).json({ error: "Method not allowed" });
+  }
+}
